d79dd74e8b58ccd21627c2aaa6bbe11f1376e90d
jatazoulja <jatazoulja@gmail.com>
2014-12-05 11:46:57 +0800

    -added switch mode for list|grid.\n -added list view capabilities with image preview

--- js/plugins.js
+++ js/plugins.js
@@ -6,13 +6,40 @@
             // Extend our default options with those provided.
             // Note that the first argument to extend is an empty
             // object – this is to keep from overriding our "defaults" object.
-            var opts = $.extend({}, $.fn.Liquid7.defaults, options), modal = _.pick(opts, 'backdrop', 'keyboard', 'show'), content = "", self = this,IMG_WIDTH = 500;
+            var opts = $.extend({}, $.fn.Liquid7.defaults, options), 
+                modal = _.pick(opts, 'backdrop', 'keyboard', 'show'), 
+                content = "", 
+                self = this,
+                IMG_WIDTH = 500;
 
             this.data = [];
             this.opts = opts;
             this.currentIndex = this.currentIndex || 0;
             this.modal = $(opts.modalContainer);
+            
+            /**
+             * Create Toggler;
+             */
+            
+            $(this.opts.toggle).on("click", function() {
+                
+                var opts = self.opts,
+                    container = $(opts.galleryContainer),
+                    switchMode = $(opts.galleryContainer).data('mode'),
+                    currentMode = switchMode.split("|");
+            
+                function switchCase() {
+                    return (currentMode[0] === "list") ? "grid" : "list"; 
+                }
+                var newMode = switchCase();
+                container.data().mode = newMode + "|" + switchMode[1];
 
+                container.toggleClass('list');
+                $(opts.previewContainer).toggleClass('hide');
+                $(opts.listContainer).toggleClass(opts.gridClass);
+                self.trigger('mode.' + newMode);
+            });
+            
             this.nextSlide = function() {
                 if (!this.opts.isContinousScrolling)
                     return;
@@ -63,27 +90,63 @@
                 },
                 threshold : 100
             }
-
+            this._checkMode = function() {
+                var modeCont = $(self.opts.galleryContainer),
+                    mode = modeCont.data('mode'),
+                    mode = mode.split('|');
+                this.mode = mode[0];
+                if(this.mode === 'grid') $(this.opts.previewContainer).addClass('hide');
+            }
             this._onClick = function(e) {
                 // this.attachClick.apply(arguments, this);
+                var prevCont = $(self.opts.previewContainer);
 
+                this._checkMode();
                 this.currentIndex = $(e.currentTarget).parent().data('index');
 
                 this._buildGallery();
-                console.log(this);
-                this.modal.modal('show');
+                
+                if(this.mode !== "list" && !prevCont.is(":visible")) {
+                    this.modal.modal('show');
+                } else {
+                    if(!prevCont.is(":visible")) {
+                        this.modal.modal('show');
+                    }
+                }
+            }; 
+            this._displayMode = function() {
+                var content = "";
+                this._checkMode();
+                                    
+                switch(this.mode) {
+                    case "grid":
+                    
+                        break;
+                    case "list":
+                        $(self.opts.galleryContainer).addClass('list');
+                        self._buildGallery()
+                        break;
+                }
             };
-
             this._buildGallery = function() {
                 if (!this.data)
                     return;
                 // this.buildingGallery.apply(arguments, this);
 
-                var content = _.template($(this.opts.template).html(), this.data[this.currentIndex]);
-
-                this.modal.html(content);
-                // initiate the modal
-                this.modal.modal(modal);
+                var content = _.template($(this.opts.template).html(), this.data[this.currentIndex]),
+                    prevCont = $(self.opts.previewContainer);
+                if(this.mode === "list") {
+                    prevCont.html(content);
+                     if(!prevCont.is(":visible")) {
+                        this.modal.html(content);
+                        // initiate the modal
+                        this.modal.modal(modal);                         
+                     }
+                } else {
+                    this.modal.html(content);
+                    // initiate the modal
+                    this.modal.modal(modal);
+                }
 
             };
             // Initiate the gallery data;
@@ -102,7 +165,12 @@
             $(opts.imageContainer).off("click", 'img').on("click", 'img', $.proxy(this._onClick, this))
 
             $(opts.modalContainer).swipe(this.swipe);
+            
+            this._displayMode();
+            
+            this.on('mode.list', $.proxy(this._displayMode, this));
 
+            
             return {
                 buildingGallery : function() {
 
@@ -147,6 +215,10 @@
             modalContainer : '#liquid-modal-gallery', // containers of the modal
             galleryContainer : '#liquid-gallery', // containst the set of images
             imageContainer : '.thumb-container', //
+            listContainer: "#list-container",
+            previewContainer: "#preview-container",
+            gridClass: "col-lg-4 col-md-4 col-sm-4 col-xs-12",
+            toggle: "#switcher",
             isContinousScrolling : true
         };
 
